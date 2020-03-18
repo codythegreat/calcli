72c0f1c91756917efdaf5b2b76987900db29326b
codythegreat <maxiecody@gmail.com>
2020-03-18 17:03:33 +0000

    shortened and l-justifies func name

--- src/debug.go
+++ src/debug.go
@@ -13,11 +13,13 @@ func getFunctionName(i interface{}) string {
 }
 
 // debug message at start of function
+// function name starts at 35th index
 func DebugMessageFunctionStart(function interface{}, output string) {
-	fmt.Printf("DEBUGGER: %s called with %s as input\n", getFunctionName(function), output)
+	fmt.Printf("DEBUGGER: %-20v called with %s as input\n", getFunctionName(function)[35:], output)
 }
 
 // debug message at end of function
+// function name starts at 35th index
 func DebugMessageFunctionEnd(function interface{}, output string) {
-	fmt.Printf("DEBUGGER: %s returned with %s as ouput\n", getFunctionName(function), output)
+	fmt.Printf("DEBUGGER: %-20v returned with %s as ouput\n", getFunctionName(function)[35:], output)
 }
