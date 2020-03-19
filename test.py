fa33153831f17c88ee13ee39728d0c2a30439ded
codythegreat <maxiecody@gmail.com>
2020-03-18 19:48:49 -0500

    added more test, total of 10

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ commands = [
     ['./calcli', '-answer', '2+2+2+2+2+2'],
     ['./calcli', '-answer', '5^{1}+20-10'],
     ['./calcli', '-answer', '18*0+3^{3}-9'],
+    ['./calcli', '-answer', '1*10+4^{2}/2+3'],
+    ['./calcli', '-answer', '48/2+8^{0}-1'],
+    ['./calcli', '-answer', '30-3'],
+    ['./calcli', '-answer', '1^{0}+2^{2}+3^{2}+4^{2}']
 ]
 
 for com in commands:
